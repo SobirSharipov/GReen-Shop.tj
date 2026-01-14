bef405f9334962a34223bc1e999eae335e395f6a
Пользователь <sobirjonsharipov590@gmail.com>
2026-01-14 12:34:28 +0500

    GReen-Shop.tj

--- src/app/Heart/heart.jsx
+++ src/app/Heart/heart.jsx
@@ -101,7 +101,7 @@ const Heart = () => {
                 className="w-full h-40 object-cover group-hover:brightness-75 transition-all duration-300"
               />
 
-              <div className="absolute inset-0 flex items-center justify-center gap-3    transition-opacity duration-300 bg-black/20">
+              <div className="absolute inset-0 flex items-center justify-center gap-3 transition-opacity duration-300 bg-black/20">
                 <button
                   onClick={(e) => {
                     e.stopPropagation();
@@ -141,7 +141,6 @@ const Heart = () => {
             <h3 className="font-semibold text-lg truncate">{el.name}</h3>
             <p className="text-[#46A358] font-bold text-xl mt-2">${el.prase}</p>
 
-            {/* Индикатор что товар в избранном */}
             <div className="absolute top-3 right-3">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#46A358" className="size-6">
                 <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
