2054607a19d7aea0acef49cf6e3a98dfea7fc788
Пользователь <sobirjonsharipov590@gmail.com>
2026-01-25 20:48:45 +0500

    GreenShop

--- src/app/Heart/heart.jsx
+++ src/app/Heart/heart.jsx
@@ -130,7 +130,7 @@ const Heart = () => {
                 <button
                   className="bg-white p-3 rounded-full shadow-lg cursor-pointer hover:bg-gray-100 transition-colors"
                   title="View details"
-                  onClick={(e) => e.stopPropagation()}
+                  
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
